fcab5ad6102e773620a7891c42eda39b0c8bc154
Siddarth Gore <siddarth.gore@gmail.com>
2023-02-09 11:54:08 +0530

    WIP: MaxEnt IRL for Iterated Prisoner's Dilemma Game
    Runs but the rewards are not correctly recovered

---
+++ src/example-ipd.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+from irl_maxent import ipdworld as W
+from irl_maxent import maxent as M
+from irl_maxent import plot as P
+from irl_maxent import trajectory as T
+from irl_maxent import solver as S
+from irl_maxent import optimizer as O
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def setup_mdp():
+    """
+    Set-up our MDP/IPDWorld
+    """
+    # create our world
+    world = W.RandomIPDWorld()
+
+    # set up the reward function
+    reward = np.zeros(world.n_states)
+    for i in range(world.n_states):
+        if world.states[i]['agt_last_act'] == 'C' and world.states[i]['opp_curr_act'] == 'C':
+            reward[i] = 5
+        elif world.states[i]['agt_last_act'] == 'D' and world.states[i]['opp_curr_act'] == 'C':
+            reward[i] = 3
+        elif world.states[i]['agt_last_act'] == 'C' and world.states[i]['opp_curr_act'] == 'D':
+            reward[i] = 0
+        elif world.states[i]['agt_last_act'] == 'D' and world.states[i]['opp_curr_act'] == 'D':
+            reward[i] = 1
+
+    # Terminate when both cooperate
+    terminal = []
+    for i, s in enumerate(world.states):
+        if s["agt_last_act"] == 'C' and s["opp_curr_act"] == 'C':
+            terminal.append(i)
+
+    return world, reward, terminal
+
+
+def generate_trajectories(world, reward, terminal):
+    """
+    Generate some "expert" trajectories.
+    """
+    # parameters
+    n_trajectories = 200
+    discount = 0.7
+    weighting = lambda x: x**5
+
+    # set up initial probabilities for trajectory generation
+    initial = np.zeros(world.n_states)
+    initial[0] = 0.5
+    initial[1] = 0.5
+
+    # generate trajectories
+    value = S.value_iteration(world.p_transition, reward, discount)
+    policy = S.stochastic_policy_from_value(world, value, w=weighting)
+    policy_exec = T.stochastic_policy_adapter(policy)
+    tjs = list(T.generate_trajectories(n_trajectories, world, policy_exec, initial, terminal))
+
+    return tjs, policy
+
+
+def maxent(world, terminal, trajectories):
+    """
+    Maximum Entropy Inverse Reinforcement Learning
+    """
+    # set up features: we use one feature vector per state
+    features = world.state_features()
+
+    # choose our parameter initialization strategy:
+    #   initialize parameters with constant
+    init = O.Constant(1.0)
+
+    # choose our optimization strategy:
+    #   we select exponentiated gradient descent with linear learning-rate decay
+    optim = O.ExpSga(lr=O.linear_decay(lr0=0.2))
+
+    # actually do some inverse reinforcement learning
+    reward = M.irl(world.p_transition, features, terminal, trajectories, optim, init)
+
+    return reward
+
+
+def maxent_causal(world, terminal, trajectories, discount=0.7):
+    """
+    Maximum Causal Entropy Inverse Reinforcement Learning
+    """
+    # set up features: we use one feature vector per state
+    features = world.state_features()
+
+    # choose our parameter initialization strategy:
+    #   initialize parameters with constant
+    init = O.Constant(1.0)
+
+    # choose our optimization strategy:
+    #   we select exponentiated gradient descent with linear learning-rate decay
+    optim = O.ExpSga(lr=O.linear_decay(lr0=0.2))
+
+    # actually do some inverse reinforcement learning
+    reward = M.irl_causal(world.p_transition, features, terminal, trajectories, optim, init, discount)
+
+    return reward
+
+
+def main():
+    # common style arguments for plotting
+    style = {
+        'border': {'color': 'red', 'linewidth': 0.5},
+    }
+
+    # set-up mdp
+    world, reward, terminal = setup_mdp()
+
+    # show our original reward
+    #ax = plt.figure(num='Original Reward').add_subplot(111)
+    #P.plot_state_values(ax, world, reward, **style)
+    #plt.draw()
+
+    # generate "expert" trajectories
+    trajectories, expert_policy = generate_trajectories(world, reward, terminal)
+
+    # show our expert policies
+    #ax = plt.figure(num='Expert Trajectories and Policy').add_subplot(111)
+    #P.plot_stochastic_policy(ax, world, expert_policy, **style)
+
+    #for t in trajectories:
+    #    P.plot_trajectory(ax, world, t, lw=5, color='white', alpha=0.025)
+
+    #plt.draw()
+
+    # maximum entropy reinforcement learning (non-causal)
+    reward_maxent = maxent(world, terminal, trajectories)
+
+    # show the computed reward
+    #ax = plt.figure(num='MaxEnt Reward').add_subplot(111)
+    #P.plot_state_values(ax, world, reward_maxent, **style)
+    #plt.draw()
+
+    # maximum casal entropy reinforcement learning (non-causal)
+    reward_maxcausal = maxent_causal(world, terminal, trajectories)
+
+    # show the computed reward
+    #ax = plt.figure(num='MaxEnt Reward (Causal)').add_subplot(111)
+    #P.plot_state_values(ax, world, reward_maxcausal, **style)
+    #plt.draw()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/irl_maxent/ipdworld.py
@@ -0,0 +1,166 @@
+"""
+IPD-World Markov Decision Processes (MDPs).
+
+The MDPs in this module are actually not complete MDPs, but rather the
+sub-part of an MDP containing states, actions, and transitions (including
+their probabilistic character). Reward-function and terminal-states are
+supplied separately.
+
+"""
+
+import numpy as np
+from itertools import product
+
+
+class IPDWorld:
+    """
+    Iterated Prisoner's Dilemma world MDP.
+
+    Args:
+        memory: how many time steps do you remember.
+
+    Attributes:
+        n_states: The number of states of this MDP.
+        n_actions: The number of actions of this MDP.
+        p_transition: The transition probabilities as table. The entry
+            `p_transition[from, to, a]` contains the probability of
+            transitioning from state `from` to state `to` via action `a`.
+        size: The width and height of the world.
+        actions: The actions of this world as paris, indicating the
+            direction in terms of coordinates.
+    """
+
+    def __init__(self, memory = 1):
+        self.memory = memory
+        self.actions = ['C', 'D']
+        self.states = []
+        
+        # initial states
+        # FIXME: Currently opponant starts. We can also make it so
+        # so that agent starts.
+        for opp_curr_act in self.actions:
+            self.states.append({"opp_last_act": 0,
+                                    "agt_last_act": 0,
+                                    "opp_curr_act": opp_curr_act})
+        # states
+        for opp_last_act in self.actions:
+            for agt_last_act in self.actions:
+                for opp_curr_act in self.actions:
+                    self.states.append({"opp_last_act": opp_last_act,
+                                        "agt_last_act": agt_last_act,
+                                        "opp_curr_act": opp_curr_act})
+
+        self.n_states = len(self.states)
+        self.n_actions = len(self.actions)
+
+        self.p_transition = self._transition_prob_table()
+
+    def _transition_prob_table(self):
+        """
+        Builds the internal probability transition table.
+
+        Returns:
+            The probability transition table of the form
+
+                [state_from, state_to, action]
+
+            containing all transition probabilities. The individual
+            transition probabilities are defined by `self._transition_prob'.
+        """
+        table = np.zeros(shape=(self.n_states, self.n_states, self.n_actions))
+
+        s1, s2, a = range(self.n_states), range(self.n_states), range(self.n_actions)
+        for s_from, s_to, a in product(s1, s2, a):
+            table[s_from, s_to, a] = self._transition_prob(s_from, s_to, a)
+
+        return table
+
+    def _is_legal(self, s_from, a, s_to):
+        if (s_to['opp_last_act'] != s_from['opp_curr_act'] or
+            s_to['agt_last_act'] != a):
+            return False
+        return True
+
+    def _index_to_values(self, s_from_idx, a_idx, s_to_idx = None):
+        if s_to_idx is not None:
+            return self.states[s_from_idx], self.actions[a_idx], self.states[s_to_idx]
+        else:
+            return self.states[s_from_idx], self.actions[a_idx]
+
+    def _values_to_index(self, s):
+        return next((idx for (idx, d) in enumerate(self.states) if d == s))
+
+    def state_features(self):
+        """
+        Return the feature matrix assigning each state with an individual
+        feature (i.e. initial_state, opp_last_act, agt_last_act, opp_curr_act).
+
+        Rows represent individual states, columns the feature entries.
+
+        Args:
+            world: An IPDWorld instance for which the feature-matrix should be
+                computed.
+
+        Returns:
+            The coordinate-feature-matrix for the specified world.
+        """
+        f = np.zeros(shape=(self.n_states, 1 + (self.memory * 2) + 1))
+        for i, s in enumerate(self.states):
+            if s["opp_last_act"] == 0:
+                #initial state
+                f[i][0] = 1
+                f[i][3] = 0 if s["opp_curr_act"] == 'C' else 1
+            else:
+                f[i][1] = 0 if s["opp_last_act"] == 'C' else 1
+                f[i][2] = 0 if s["agt_last_act"] == 'C' else 1
+                f[i][3] = 0 if s["opp_curr_act"] == 'C' else 1
+        return f
+
+    def __repr__(self):
+        return "IPDWorld(memory={})".format(self.memory)
+
+class RandomIPDWorld(IPDWorld):
+    """
+    The opponent is a random player who chooses cooprate
+    or defect with same probability.
+    """
+    def _transition_prob(self, s_from_idx, s_to_idx, a_idx):
+        """
+        Compute the transition probability for a single transition.
+
+        Args:
+            s_from: The state in which the transition originates.
+            s_to: The target-state of the transition.
+            a: The action via which the target state should be reached.
+
+        Returns:
+            The transition probability from `s_from` to `s_to` when taking
+            action `a`.
+        """
+        s_from, a, s_to = self._index_to_values(s_from_idx, a_idx, s_to_idx)
+        if not self._is_legal(s_from, a, s_to):
+            return 0.0
+        # Use a random policy for now so half probabilty of choosing either of the two actions
+        return 0.5
+    
+    def state_index_transition(self, s_idx, a_idx):
+        """
+        Perform action `a` at state `s` and return the intended next state.
+
+        Does not take into account the transition probabilities. Instead it
+        just returns the intended outcome of the given action taken at the
+        given state, i.e. the outcome in case the action succeeds.
+
+        Args:
+            s: The state at which the action should be taken.
+            a: The action that should be taken.
+
+        Returns:
+            The next state as implied by the given action and state.
+        """
+        s, a = self._index_to_values(s_idx, a_idx)
+        s_next = {"opp_last_act": s["opp_curr_act"],
+                  "agt_last_act": a,
+                  # Opponent is playing uniform random policy
+                  "opp_curr_act": np.random.choice(self.actions)}
+        return self._values_to_index(s_next)
